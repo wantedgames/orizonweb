ee907e0f546c4f9e39a82cb1b9d1afa66b27e7c7
Skeletoos57 <89609124+Skeletoos57@users.noreply.github.com>
2022-07-18 17:28:08 -0300

    Update button.js

--- button.js
+++ button.js
@@ -24,7 +24,7 @@ returnprogress.addEventListener('click', () => {
     }else if (password == "0609xxx") { // Taiel
         returnProgress("1");
     }else if (password == "NaGiiTO548903") { // Barbie
-        returnProgress("7");
+        returnProgress("6");
     }else if (password == "turron") { // Uriel
         returnProgress("3");
     }else{
