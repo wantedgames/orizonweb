02200db3956177f78f13d1d637a91d4627fc899a
Skeletoos57 <89609124+Skeletoos57@users.noreply.github.com>
2022-07-18 17:48:10 -0300

    Update button.js

--- button.js
+++ button.js
@@ -24,7 +24,8 @@ returnprogress.addEventListener('click', () => {
     }else if (password == "0609xxx") { // Taiel
         returnProgress("1");
     }else if (password == "NaGiiTO548903") { // Barbie
-        returnProgress("6");
+        alert("Esta contraseña fue suspendida hasta el Jueves");
+        // returnProgress("6");
     }else if (password == "turron") { // Uriel
         returnProgress("5");
     }else{
